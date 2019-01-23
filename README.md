ff25f370f7eb6e23b318171731ae34e45641cf46
G. Zechariah McPherson <ZechariahMcPherson@gmail.com>
2019-01-23 09:39:54 -0600

    Created schema and made README

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # mini-graphql-project
+
+Building out mini graphql project via [this tutorial](https://www.youtube.com/playlist?list=PL4cUxeGkcC9iK6Qhn-QLcXCXPQUov1U7f)
+
+## How to run project
+
+### Install node modules
+
+```
+yarn install
+```
+
+### start project
+
+```
+yarn start
+```
+
+### url
+
+```
+http://localhost:4000/graphql?
+```
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,25 @@
 const graphql = require("graphql");
+const _ = require("lodash");
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+//dummy data
+const books = [
+  { name: "book one", genre: "Fantasy", id: "1" },
+  { name: "book two", genre: "Fantasy", id: "2" },
+  { name: "book three", genre: "Fantasy", id: "3" },
+  { name: "book four", genre: "Fantasy", id: "4" },
+  { name: "book five", genre: "Fantasy", id: "5" },
+  { name: "book six", genre: "Fantasy", id: "6" },
+  { name: "book seven", genre: "Fantasy", id: "7" },
+  { name: "book eight", genre: "Sci-Fi", id: "8" },
+  { name: "book nine", genre: "Sci-Fi", id: "9" },
+  { name: "book ten", genre: "Sci-Fi", id: "10" },
+  { name: "book eleven", genre: "Sci-Fi", id: "11" },
+  { name: "book twelve", genre: "Sci-Fi", id: "12" },
+  { name: "book thirteen", genre: "Sci-Fi", id: "13" }
+];
 
+//object type bookType
 const BookType = new GraphQLObjectType({
   name: "Book",
   fields: () => ({
@@ -9,4 +27,22 @@ const BookType = new GraphQLObjectType({
     name: { type: GraphQLString },
     genre: { type: GraphQLString }
   })
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        //code to get data from db/ other source
+        return _.find(books, { id: args.id });
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 });
